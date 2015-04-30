e3e610a5844e868b925a8e03414cb279dc4ef3ce
Max Azarcon <max@trinitysedge.com>
2015-04-30 00:57:44 -0400

    test

--- js/overlay.js
+++ js/overlay.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
 	$(".overlay").hide();
 	$("#operators").mouseenter(function() {
-		$("#operatorsOverlay").show("slow");
+		$("#operatorsOverlay").delay(2000).show("slow");
+		$(".row").delay(1700).hide("slow");
 	});
 	
 	$("#mros").mouseenter(function(){
-		$("#mrosOverlay").show("slow");
+		$("#mrosOverlay").delay(2000).show("slow");
+		$(".row").delay(1700).hide("slow");
 	});
 	
 	$("#lessors").mouseenter(function(){
-		$("#lessorsOverlay").show("slow");
+		$("#lessorsOverlay").delay(2000).show("slow");
+		$(".row").delay(1700).hide("slow");
 	});
 	
 	$("#oems").mouseenter(function(){
-		$("#oemsOverlay").show("slow");
+		$("#oemsOverlay").delay(2000).show("slow");
+		$(".row").delay(1700).hide("slow");
 	});
 	
 	$(".overlay").click(function(){
 		$(this).hide("slow");
+		$(".row").show("slow");
 	});
 }); 
